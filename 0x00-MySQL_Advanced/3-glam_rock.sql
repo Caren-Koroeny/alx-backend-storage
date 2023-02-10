a3be4e6c211d8a29e15e506a4c1aaef69d939182
Caren-Koroeny <carensiya5@gmail.com>
2023-02-10 01:47:44 +0000

    mysql files

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- This SQL script select band_name, and lifespan column which is difference
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
